2c8d96467d60e43d69f71a3addccea538aba8a30
Frella-dev <analyst@frellainternational.com>
2026-06-16 13:24:32 +0530

    Update sheets.py

--- sheets.py
+++ sheets.py
@@ -1,12 +1,16 @@
 import pandas as pd
+import re
 
 def load_sheet(sheet_url):
 
-    csv_url = sheet_url.replace(
-        "/edit?usp=sharing",
-        "/export?format=csv"
+    sheet_id = re.search(
+        r"/d/([a-zA-Z0-9-_]+)",
+        sheet_url
+    ).group(1)
+
+    csv_url = (
+        f"https://docs.google.com/spreadsheets/d/"
+        f"{sheet_id}/export?format=csv"
     )
 
-    df = pd.read_csv(csv_url)
-
-    return df
+    return pd.read_csv(csv_url)
